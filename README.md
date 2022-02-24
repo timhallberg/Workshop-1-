6d891ff197e736a7898022c128d25fea07524ec1
timhallberg <tim.hallberg@outlook.com>
2022-02-24 10:07:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-tprnpb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-tprnpb)
---
+++ script.js
@@ -0,0 +1,26 @@
+// 1. Iterera över arrayen const numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"] och skriv för varje element i arrayen ut hur lång strängen är (dvs. hur många tecken strängen omfattar). Längden på en sträng ges av attributet .length. Skriv ut längderna i ett HTML-element eller använd console.log för detta syfte.
+
+const numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+
+// console.log(numbers [2].length);
+
+for (i=0 ; i < numbers; i++) {
+console.log (i)
+}
+
+
+//2. Skriv ut de tal i arrayen numbers som är på jämna positioner i arrayen, dvs positionerna 0, 2, 4 osv. Tips: för att avgöra om ett tal är jämnt, dvs. jämnt delbart med 2, kan vi använda operatorn % som betyder "modulo". Givet en variabel x gäller att om x % 2 === 0 är den s.k. resten vid heltalsdivision med 2 lika med 0, vilket innebär att x är ett jämnt tal.
+
+// const numbers = ["one", "two", "three", "four", "five", 
+// "six", "seven", "eight", "nine", "ten"];
+
+
+
+
+//Skriv ut summan av alla jämna heltal från 1 till 20. 
+
+
+
+
+
+// console.log(numbers.filter((x, i) => i % 2===1));
